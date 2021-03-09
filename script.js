6fd42d688a50ee65a29c2bc63d6b9c7b21d49700
shoneroc <pinotti.nate@gmail.com>
2021-03-08 19:47:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    var principal = p;
+    
+    q = document.getElementById("rate").value;
+    var rate = q;
+    
+    m = document.getElementById("years").value;
+    var years = m
+    
+    var interest = (principal*years*rate)/100
+    
+    
+    
     
 }
         
