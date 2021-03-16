c585f2f499c6c65da8dbfe0701ec32f0954d63e8
shoneroc <pinotti.nate@gmail.com>
2021-03-15 20:13:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,48 @@
     function compute()
 {
     p = document.getElementById("principal").value;
-    var principal = p;
+    var principal = number(p);
     
     q = document.getElementById("rate").value;
-    var rate = q;
+    var rate = number(q);
     
     m = document.getElementById("years").value;
-    var years = m;
+    var years = number(m);
     
-    var interest = (principal*years*rate)/100;
+    var interest = principal*years*rate/100;
     
-    var d = new.Date();
-    var future_years = d.getFullYear() + years;
+    if (validatePrincipal() === true) {
+
+        var currentDate = new Date();
+        var currentYear = currentDate.getFullYear();
+        currentYear = Number(currentYear);
+
+        var futureYear = currentYear + years;
+
+        var result = document.getElementById("result");
+
+        var output = "If you deposit <mark>" + principal + "</mark>,<br/>" + "at an interest rate of <mark>" + rate + "</mark>%,<br/>" + "You will receive an amount of <mark>" + interest + "</mark>,<br/> in the year <mark>" + futureYear + "</mark>";
+
+        result.innerHTML = output;
     
-    
-    
-    
+    }
 }
+
+function showRange() 
+{
+    var rate = document.getElementById("rate").value;
+    document.getElementById("rate").nextElementSibling.innerHTML = rate + ' %';
+}
+function validatePrincipal()
+{
+    var principal = document.getElementById("principal").value;
+    var errorMessage = 'Enter a positive number'
+    if ((principal == 0) || (principal < 0)) {
+        if (confirm(errorMessage)) {
+            document.getElementById("principal").focus();
+        }
+    } else {
+        return true;
+    }
+}
+
